7fa58c6d59acb2857afc343525c50eb08452fbae
monsieurferaud <monsieurferaud@gmail.com>
2017-12-25 21:54:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test BBDD</title>
+</head>
+<body>
+	<?php
+try {
+	$usuario = "root";
+	$contrasena = "";
+    $mbd = new PDO('mysql:host=localhost;dbname=planos', $usuario, $contrasena);
+    foreach($mbd->query('SELECT * from planos') as $fila) {
+        print_r($fila);
+    }
+    $mbd = null;
+} catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+</body>
+</html>
